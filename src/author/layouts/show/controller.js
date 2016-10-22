da028e96a6f253f92c02f96b29a2271af070fa76
Tatum Szymczak <tatum@ashlandstudios.com>
2016-10-22 14:43:12 -0500

    remove layouts show page

--- src/author/layouts/show/controller.js
+++
@@ -1,9 +0,0 @@
-export default class controller {
-  constructor(AwsService, item, id) {
-    this.id = id
-    this.title = "Layout Show"
-    this.item = item
-    this.body = item.Body.toString()
-  }
-}
-controller.$inject = ['AwsService', 'item', 'id']
--- src/author/layouts/show/index.js
+++
@@ -1,9 +0,0 @@
-import routes from './routes'
-import controller from './controller'
-
-const component = angular.module('app.layouts.show', [])
-  .config(routes)
-  .controller('LayoutsShowController', controller)
-  .name
-
-export default component
