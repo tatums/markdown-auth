a218987c0c10448a4c546e20f19254cc35acc768
Tatum Szymczak <tatum@ashlandstudios.com>
2016-10-29 12:50:58 -0500

    add page create and page delete fns

--- src/author/pages/index/controller.js
+++ src/author/pages/index/controller.js
@@ -1,7 +1,40 @@
+const defaultBody = `---
+title: A blog post
+date: 2016-01-21
+layout: post.html
+---
+An interesting post.
+`
+
 class controller {
-  constructor(pages) {
+
+  constructor(AwsService, pages) {
     this.pages = pages
+    this.AwsService = AwsService
   }
+
+  create (form, validity) {
+    let key = `admin/src/pages/${form.key}`
+    this.AwsService.putObject(key, defaultBody)
+      .then((resp) => {
+        let date = new Date()
+        this.items.unshift({ Key: key, LastModified: date })
+        console.log(resp)
+      }).catch((err) => {
+        console.log(err)
+      })
+  }
+
+  delete (page) {
+    let index = this.pages.indexOf(page)
+    this.pages.splice(index, 1)
+    this.AwsService.deleteObject(page.Key)
+      .then((resp) => {
+      }).catch((err) => {
+        console.log('err: ', err)
+      })
+  }
+
 }
-controller.$inject = ['pages']
+controller.$inject = ['AwsService', 'pages']
 export default controller
