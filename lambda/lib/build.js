2147c70cbc6df6584461661b3ff6c021ff0a8aa1
Tatum Szymczak <tatum@ashlandstudios.com>
2016-11-06 08:01:21 -0600

    update metalsmith to work with collections

--- lambda/lib/build.js
+++ lambda/lib/build.js
@@ -1,12 +1,15 @@
 const Metalsmith = require('metalsmith')
 const markdown = require('metalsmith-markdown')
+const collections = require('metalsmith-collections')
+const permalinks  = require('metalsmith-permalinks')
 const layouts = require('metalsmith-layouts')
+const discoverPartials = require('metalsmith-discover-partials')
 
 module.exports = () => {
 
   const sourcePath = '/tmp/admin/src'
 
-  return new Promise(function (resolve, reject) {
+  return new Promise((resolve, reject) => {
     Metalsmith(__dirname)
       .metadata({
         title: "Hello",
@@ -14,14 +17,26 @@ module.exports = () => {
       })
       .source(sourcePath)
       .destination('/tmp/build')
-      .clean(true)
+      .clean(false)
+      .use(discoverPartials({
+        directory: "/tmp/admin/layouts",
+        pattern: /\.hbs$/
+      }))
+      .use(collections({
+        posts: {
+          pattern: 'posts/*.md',
+          sortBy: 'date',
+          reverse: true
+        }
+      }))
       .use(markdown())
+      .use(permalinks())
       .use(layouts({
         engine: 'handlebars',
         directory: "/tmp/admin/layouts"
       }))
-      .build(function(err, files) {
-        if (err) { reject(err) }
+      .build((err, files) => {
+        if (err) { reject(err); console.log(`\n\n ${err} \n\n`); return; }
         console.log('\nbuild complete\n')
         resolve(files)
       })
